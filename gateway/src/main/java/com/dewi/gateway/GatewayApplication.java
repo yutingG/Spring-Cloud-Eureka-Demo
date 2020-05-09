c4c9e50705fbedd193eddf07333b84b6815c106c
yutingGuo <13281030@bjtu.edu.cn>
2020-05-09 16:31:49 +0800

    添加：zipkin模块，并为gateway、consumer、provider添加zipkin依赖

--- gateway/src/main/java/com/dewi/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/dewi/gateway/GatewayApplication.java
@@ -2,22 +2,24 @@ package com.dewi.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
-	/**
-	 * 方式二：采用代码形式，配置转发规则
-	 * @param builder
-	 * @return
-	 */
+//	/**
+//	 * 方式二：采用代码形式，配置转发规则
+//	 * @param builder
+//	 * @return
+//	 */
 //	@Bean
 //	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 //		return builder.routes()
